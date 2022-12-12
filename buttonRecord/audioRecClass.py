dd43204f4651d9b9ee9e278dad98907472b289a1
Connorbelez <connor.belez@gmail.com>
2022-12-11 15:26:39 -1000

    Quick Commit

--- buttonRecord/audioRecClass.py
+++ buttonRecord/audioRecClass.py
@@ -1,102 +1,121 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import time
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import argparse
+import tempfile
+import queue
+import sys
+import os
+import sounddevice as sd
+import soundfile as sf
+import numpy  # Make sure NumPy is loaded before it is used in the callback
+assert numpy  # avoid "imported but unused" message (W0611)
+import select 
 
 
 class audioRecorder:
     def __init__(self):
         
         self.breakFlag = False
-        
-        self.parser = argparse.ArgumentParser(add_help=False)
-        self.parser.add_argument(
-            '-l', '--list-devices', action='store_true',
-            help='show list of audio devices and exit')
-        
-        self.args, self.remaining = parser.parse_known_args()
-        
-        if self.args.list_devices:
-            print(sd.query_devices())
-            parser.exit(0)
-        self.parser = argparse.ArgumentParser(
-            description=__doc__,
-            formatter_class=argparse.RawDescriptionHelpFormatter,
-            parents=[parser])
-        self.parser.add_argument(
-            'filename', nargs='?', metavar='FILENAME',
-            help='audio file to store recording to')
-        self.parser.add_argument(
-            '-d', '--device', type=int_or_str,
-            help='input device (numeric ID or substring)')
-        self.parser.add_argument(
-            '-r', '--samplerate', type=int, help='sampling rate')
-        self.parser.add_argument(
-            '-c', '--channels', type=int, default=1, help='number of input channels')
-        self.parser.add_argument(
-            '-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
-        self.args = self.parser.parse_args(remaining)
-        
         self.q = queue.Queue()
         
+
+        
+    def int_or_str(self,text):
+        try:
+            return int(text)
+        except ValueError:
+            return text
+        
+    
     def callback(self,indata,frames,time,status):
         """This is called (from a separate thread) for each audio block."""
         if status:
             print(status, file=sys.stderr)
-        q.put(indata.copy())
+        self.q.put(indata.copy())
 
-    def beginRecording(self,pipe_fd=None):
+    def initialize(self,pipe_name=None):
+        parser = argparse.ArgumentParser(add_help=False)
+        parser.add_argument(
+            '-l', '--list-devices', action='store_true',
+            help='show list of audio devices and exit')
         
-        try:
+        args, remaining = parser.parse_known_args()
+        # q = queue.Queue()
+
+        if args.list_devices:
+            print(sd.query_devices())
+            parser.exit(0)
+        parser = argparse.ArgumentParser(
+            description=__doc__,
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            parents=[parser])
+        parser.add_argument(
+            'filename', nargs='?', metavar='FILENAME',
+            help='audio file to store recording to')
+        parser.add_argument(
+            '-d', '--device', type=self.int_or_str,
+            help='input device (numeric ID or substring)')
+        parser.add_argument(
+            '-r', '--samplerate', type=int, help='sampling rate')
+        parser.add_argument(
+            '-c', '--channels', type=int, default=1, help='number of input channels')
+        parser.add_argument(
+            '-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
+        args = parser.parse_args(remaining)
+        
+
+        
+        pipe_fd = os.open(pipe_name, os.O_RDONLY)
+        
+        
+        while True:
+    
             if args.samplerate is None:
                 device_info = sd.query_devices(args.device, 'input')
                 # soundfile expects an int, sounddevice provides a float:
                 args.samplerate = int(device_info['default_samplerate'])
-            if args.filename is None:
-                args.filename = tempfile.mktemp(prefix='delme_rec_unlimited_',
-                                                suffix='.wav', dir='')
+            # if args.filename is None:
+            #     args.filename = tempfile.mktemp(prefix='delme_rec_unlimited_',
+            #                                     suffix='.wav', dir='')
 
             # Make sure the file is opened before recording anything:
-            with sf.SoundFile(args.filename, mode='x', samplerate=args.samplerate,
-                            channels=args.channels, subtype=args.subtype) as file:
-                with sd.InputStream(samplerate=args.samplerate, device=args.device,
-                                    channels=args.channels, callback=callback):
-                    print('#' * 80)
-                    print('press Ctrl+C to stop the recording')
-                    print('#' * 80)
-                    while True:
-                        if pipe_fd:
-                            r, w, e = select.select([pipe_fd], [], [],0)
-                            # print("R: ",r)
-                            # print('.')
-                            if pipe_fd in r:
-                                l = str(os.read(pipe_fd,1024))
-                                l = l.strip("b").strip("'")
-                                print(l)
-                                if l == "R":
-                                    raise
-                                    break
-                            message = os.read(pipe_fd, 1024)
-                            print("MESSAGE FROM AR: ",message)
-                        file.write(q.get())
-        except KeyboardInterrupt:
-            print('\nRecording finished: ' + repr(args.filename))
-            parser.exit(0)
-        except Exception as e:
-            parser.exit(type(e).__name__ + ': ' + str(e))
+            code = str(os.read(pipe_fd,1024))
+            print(code)
+            code = code.strip("b").strip("'")
+            self.q.queue.clear()
+            if "P" in code:
+                print("Starting Recording...")
+                args.filename = tempfile.mktemp(prefix='delme_rec_unlimited_',
+                                                    suffix='.wav', dir='')
+            
+                with sf.SoundFile(args.filename, mode='x', samplerate=args.samplerate,
+                                channels=args.channels, subtype=args.subtype) as file:
+                    with sd.InputStream(samplerate=args.samplerate, device=args.device,
+                                        channels=args.channels, callback=self.callback):
+                        print('#' * 80)
+                        print('press Ctrl+C to stop the recording')
+                        print('#' * 80)
+                        while True:
+                            if pipe_fd:
+                                r, w, e = select.select([pipe_fd], [], [],0)
+                                if pipe_fd in r:
+                                    l = str(os.read(pipe_fd,1024))
+                                    l = l.strip("b").strip("'")
+                                    print(l)
+                                    if l == "R":
+                                        print("Recording Finished: ",repr(args.filename))
+                                        
+                                        
+                                        #publish name of file and instructions.
+                                        break
+                                # message = os.read(pipe_fd, 1024)
+                                # print("MESSAGE FROM AR: ",message)
+                            file.write(self.q.get())
+        # except KeyboardInterrupt:
+        #     print('\nRecording finished: ' + repr(args.filename))
+        #     parser.exit(0)
+        # except Exception as e:
+        #     parser.exit(type(e).__name__ + ': ' + str(e))
             
     
--- buttonRecord/main.py
+++ buttonRecord/main.py
@@ -1,4 +1,5 @@
-from myLib import Button, audioRecorder, testClass
+from myLib import Button, testClass
+from audioRecClass import audioRecorder
 import threading
 import sys
 import os
@@ -15,14 +16,14 @@ else:
     os.remove(pipe_name)
     os.mkfifo(pipe_name, 0o666)
 b = Button()
-r = testClass()
+r = audioRecorder()
 t1 = threading.Thread(target=b.listen, args=(pipe_name,))
-t2 = threading.Thread(target=r.loop, args=(pipe_name,))
+t2 = threading.Thread(target=r.initialize, args=(pipe_name,))
 
 t2.start()
 t1.start()
 # b.listen(pipe_name)
-print("entering whileLoop")
+# print("entering whileLoop")
 # while True:
 #     message = os.read(pipe_fd, 1024)
 #     print("Received messageasdf:", message)
